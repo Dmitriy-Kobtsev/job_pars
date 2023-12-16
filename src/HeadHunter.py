3cc533f5bbd57c951d1ca8321d9a6d4fd4157dfb
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2023-12-17 00:12:00 +0300

    изменил класс HH, убрал лишнее

--- src/HeadHunter.py
+++ src/HeadHunter.py
@@ -2,37 +2,24 @@ from src.API_job_site import ApiJob
 import requests
 
 class HeadHanterAPI(ApiJob):
+
     url = "https://api.hh.ru/vacancies"
 
-    def get_vacancies(self, keyword):
+    def get_vacancies(self, keyword, per_page):
         params = {
             "text": keyword,
-            "area": 1,  # Specify the desired area ID (1 is Moscow)
-            "per_page": 2,  # Number of vacancies per page
+            "area": 113,  # Specify the desired area ID (1 is Moscow)
+            "per_page": per_page,  # Number of vacancies per page
         }
         headers = {
             "User-Agent": "kdm007@mail.ru",
         }
 
-        response = requests.get(self.url, params=params, headers=headers)
+        response = requests.get(HeadHanterAPI.url, params=params, headers=headers)
 
         if response.status_code == 200:
             data = response.json()
-            print(data)
-            print('____')
             vacancies = data.get("items", [])
-            for vacancy in vacancies:
-                print(vacancy)
-                self.vacancy_id = vacancy.get("id")
-                self.vacancy_title = vacancy.get("name")
-
-                self.vacancy_url = vacancy.get("alternate_url")
-                self.company_name = vacancy.get("employer", {}).get("name")
-                if vacancy.get('salary') == None :
-                    self.vacancy_salary = None
-                else:
-                    self.vacancy_salary = f"{vacancy.get('salary')['from']}-{vacancy.get('salary')['to']}"
-                #print(f"ID: {self.vacancy_id}\nTitle: {self.vacancy_title}\nCompany: {self.company_name}\nURL: {self.vacancy_url}\n")
-                print('salary: ', self.vacancy_salary)
+            return vacancies
         else:
             print(f"Request failed with status code: {response.status_code}")
