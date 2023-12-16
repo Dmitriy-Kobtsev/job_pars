fd59191e1c4beb2b6b3424f86cea0ac435793f0e
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2023-12-17 00:13:48 +0300

    начал добавлять взаимодействие с пользователем

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 from src.HeadHunter import HeadHanterAPI
+from src.vacancies import Vacancy
+from src.Saved_json import SaveJson
 
 if __name__ == '__main__':
     hh = HeadHanterAPI()
-    hh.get_vacancies('python')
+    json_saver = SaveJson()
+    search_query = input('Введите поисковый запрос: ')
+    N_need_vacancies = int(input('Введите необходимое количество вакансий: '))
+    vacancies = hh.get_vacancies(search_query, N_need_vacancies)
+    for data in vacancies:
+        if data.get('salary') == None:
+            vacancy_salary = None
+        else:
+            vacancy_salary = f"{data.get('salary')['from']}-{data.get('salary')['to']}"
+
+        vacancy = Vacancy(
+            data.get("id"),
+            data.get("name"),
+            data.get("alternate_url"),
+            vacancy_salary,
+            data.get("employer", {}).get("name"),
+            data.get("snippet", {}).get("responsibility"),
+            data.get("snippet", {}).get("requirement")
+        )
+        print(vacancy)
+        json_saver.add_vacancy(vacancy)
+
+
+    top_n = int(input('Введите количество вакансий для вывода в топ N: '))
